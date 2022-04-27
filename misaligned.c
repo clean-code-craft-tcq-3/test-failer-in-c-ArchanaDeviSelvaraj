97b0c326070751e761a5216aa451b40c1f00ac14
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-27 18:06:23 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
+#include <string.h>
+
+const char* colorpair[];
 
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    const char* colorpair[];
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {

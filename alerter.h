6f114e49de8a18f456693322f533273a1ea823e1
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-28 17:27:26 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -5,4 +5,35 @@ int network(float);
 float farenheit;
 float celcius;
 int returncode;
-
+int stub(celcius)
+{
+    // Return 200 for ok
+    // Return 500 for not-ok
+    if(celcius > 0)
+    {
+        return 200;
+    }
+    else
+    {
+        return 500;
+    }
+}
+int network(celcius)
+{
+    // Return 200 for ok
+    // Return 500 for not-ok
+    if(celcius > 0)
+    {
+        return 200;
+    }
+    else
+    {
+        return 500;
+    }
+}
+float FarenheitToCelcius(float farenheit) 
+{
+    celcius = (farenheit - 32) * 5 / 9;
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    return celcius;
+ }

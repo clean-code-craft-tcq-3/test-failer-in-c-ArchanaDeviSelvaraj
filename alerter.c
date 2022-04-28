1c41170514fc4232df20447ee83d7762a0aee56d
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-28 15:49:45 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -27,9 +27,9 @@ int alertFailureCount = 0;
     }
 
 int main() {
-    environment="Test";
+    environment[15]="Test";
     CheckalertFailureCount(400.5);
-    environment="Integration";
+    environment[15]="Integration";
     CheckalertFailureCount(303.6);
     CheckalertFailureCount(20);
     printf("%d alerts failed.\n", alertFailureCount);

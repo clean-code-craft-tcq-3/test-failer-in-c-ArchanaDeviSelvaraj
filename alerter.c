858d493f2a6e07afb2025ad4214165d9417c8ba7
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-28 14:35:04 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -3,8 +3,7 @@
 #include "FarenheitToCelsius.h"
 
 int alertFailureCount = 0;
-
-int returnCode = FarenheitToCelsius(farenheit)
+int returnCode = FarenheitToCelsius(farenheit);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
@@ -12,7 +11,6 @@ int returnCode = FarenheitToCelsius(farenheit)
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
     }
-}
 
 int main() {
     FarenheitToCelsius(400.5);

8f901541087376bb2ade831c3d0bbb15a06e1a4a
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-28 12:48:53 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -25,7 +25,7 @@ int main() {
     printf("%s|%s\n", majorColorlist[0], minorColorlist[0]);
     printf("%s|%s\n", majorColorlist[3], minorColorlist[3]);
     printf("%s|%s\n", majorColorlist[15], minorColorlist[15]);
-    strcmp(majorColorlist[0],"White");
+    strcmp(majorColorlist[0], "White");
     assert(result == 25);
     //assert(result == 0);
     //assert(strcmp(majorColorlist[0],"White"));

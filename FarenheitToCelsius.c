9848ad4f9120f29bef261eb45f576f227b45414e
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-28 14:31:49 +0530

    Update FarenheitToCelsius.c

--- FarenheitToCelsius.c
+++ FarenheitToCelsius.c
@@ -1,5 +1,17 @@
-float FarenheitToCelsius(float farenheit) {
+int FarenheitToCelsius(float farenheit) 
+{
     float celcius = (farenheit - 32) * 5 / 9;
-    return celcius;
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    if(celcius > 0)
+    {
+        return 200;
+    }
+    else
+    {
+        return 500;
     }
 }
+

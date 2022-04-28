4d0202862b0b1f65e4ef6887dd63669a7c245c26
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-28 14:45:07 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -12,6 +12,7 @@ int returnCode = FarenheitToCelsius(farenheit);
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
         alertFailureCount += 0;
+        }
     }
 
 int main() {

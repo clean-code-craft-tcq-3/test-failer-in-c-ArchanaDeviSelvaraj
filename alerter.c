41866c63ff5c3811d4ded9bcc1c375791ed70e99
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-04-28 14:43:52 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -4,7 +4,9 @@
 
 int alertFailureCount = 0;
 int returnCode = FarenheitToCelsius(farenheit);
-    if (returnCode != 200) {
+    int CheckalertFailureCount(returnCode)
+    {
+        if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
@@ -16,6 +18,7 @@ int main() {
     FarenheitToCelsius(400.5);
     FarenheitToCelsius(303.6);
     FarenheitToCelsius(20);
+    CheckalertFailureCount(returnCode)[;
     printf("%d alerts failed.\n", alertFailureCount);
     assert(alertFailureCount == 1);
     printf("All is well (maybe!)\n");

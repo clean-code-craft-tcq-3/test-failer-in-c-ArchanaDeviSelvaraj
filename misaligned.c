ab0be8660ddb92650feebb252d9b50b48bc157b3
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-02 17:57:50 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -30,14 +30,16 @@ int printColorMap() {
 
 int main() {
     int result = printColorMap();
+    assert(result == 25);
     printf("Pair number  1 : %-6s | %-6s\n", majorColorlist[0], minorColorlist[0]);
     printf("Pair number  4 : %-6s | %-6s\n", majorColorlist[3], minorColorlist[3]);
     printf("Pair number 16 : %-6s | %-6s\n", majorColorlist[15], minorColorlist[15]);
     const char* paironemajorcolor = "White";
-    //const char* pairtwominorcolor = "Orange";
+    const char* pairtwominorcolor = "Orange";
+    //Test cases to verify colorpair
     assert(majorColorlist[0]==paironemajorcolor);
-    //assert(minorColorlist[1]==pairtwominorcolor);
-    assert(result == 25);
+    assert(minorColorlist[1]==pairtwominorcolor);
+    //Test case to check string alignment
     assert(misalignederrorcount==0);
     printf("All is well (maybe!)\n");
     return 0;

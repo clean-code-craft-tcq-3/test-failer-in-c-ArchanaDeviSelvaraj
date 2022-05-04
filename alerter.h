1ba48a6dda920225d9f9e65508901d931dc16315
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-04 16:04:41 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -7,7 +7,7 @@ void AlertFarenheitToCelcius(float farenheit,int alertFailureCount,int (*network
                 // let us keep a count of failures to report
                 // However, this code doesn't count failures!
                 // Add a test below to catch this bug. Alter the stub above, if needed.
-                alertFailureCount += 0;
+                alertFailureCount += 1;
             }
 }
 

f997daacb92ab3ba41174f53e4de88ccedbf4ca8
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-04 15:54:16 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -3,37 +3,15 @@
 #include "alerter.h"
 
 int alertFailureCount = 0;
-enum environments{Test,Integration}environment;
-
-    void AlertFarenheitToCelcius(float farenheit) 
-    {
-        celcius = (farenheit - 32) * 5 / 9;
-        printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-        if (environment == Test)
-        {
-            returncode = stub(celcius);
-        }
-        else
-        {
-            //Integration environment
-            returncode = network(celcius);
-        }
-        if (returncode != 200) 
-        {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-        }
-    }
+float farenheit;
+float celcius;
 
 int main() {
-    environment=Test;
-    AlertFarenheitToCelcius(400.5);
-    environment=Integration;
-    AlertFarenheitToCelcius(303.6);
-    AlertFarenheitToCelcius(20);
+    
+    AlertFarenheitToCelcius(400.5,alertFailureCount,networkAlertInterface);
+    AlertFarenheitToCelcius(303.6,alertFailureCount,networkAlertStub);
+    AlertFarenheitToCelcius(20,alertFailureCount,network);
+    AlertFarenheitToCelcius(10,alertFailureCount,networkAlertInterface);
     printf("%d alerts failed.\n", alertFailureCount);
     assert(alertFailureCount == 1);
     printf("All is well (maybe!)\n");

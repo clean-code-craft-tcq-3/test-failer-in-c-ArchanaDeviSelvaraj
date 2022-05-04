ac76e3ea2b4f2c2cb708f36e084a1f4569e9c3bf
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-04 15:54:15 +0530

    Update alerter.h

--- alerter.h
+++ alerter.h
@@ -1,20 +1,44 @@
-void FarenheitToCelsius(float farenheit) 
-{
-    celcius = (farenheit - 32) * 5 / 9;
-    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
- }
-
 void AlertFarenheitToCelcius(float farenheit,int alertFailureCount,int *network) 
 {
-        celcius = FarenheitToCelsius (celcius);
-        printf("ALERT: Temperature is %.1f celcius.\n", celcius);
-       
-        if (returncode != 200) 
-        {
-        // non-ok response is not an error! Issues happen in life!
-        // let us keep a count of failures to report
-        // However, this code doesn't count failures!
-        // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
-        }
+         float celcius = (farenheit - 32) * 5 / 9;
+         int returnCode = network(celcius);
+         if (returnCode != 200) {
+                // non-ok response is not an error! Issues happen in life!
+                // let us keep a count of failures to report
+                // However, this code doesn't count failures!
+                // Add a test below to catch this bug. Alter the stub above, if needed.
+                alertFailureCount += 0;
+            }
 }
+
+int networkAlertStub(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    if(celcius > 0)
+    {
+        return 200;
+    }
+    else
+    {
+        return 500;
+    }
+}
+
+int networkAlertInterface(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    // stub always succeeds and returns 200
+    if(celcius > 0)
+    {
+        return 200;
+    }
+    else
+    {
+        return 500;
+    }
+}
+
+

55c60319d3cec98a5928479c2b5dca2f131a9c29
ArchanaDeviSelvaraj <100116449+ArchanaDeviSelvaraj@users.noreply.github.com>
2022-05-09 19:21:14 +0530

    Update alerter.c

--- alerter.c
+++ alerter.c
@@ -11,9 +11,10 @@ int main() {
     alertFailureCount = AlertFarenheitToCelcius(400.5,alertFailureCount,networkAlertInterface);
     alertFailureCount = AlertFarenheitToCelcius(303.6,alertFailureCount,networkAlertStub);
     alertFailureCount = AlertFarenheitToCelcius(20,alertFailureCount,networkAlertInterface);
+     assert(alertFailureCount == 1 );
     alertFailureCount = AlertFarenheitToCelcius(10,alertFailureCount,networkAlertStub);
     printf("%d alerts failed.\n", alertFailureCount);
-    assert(alertFailureCount > 0);
+    assert(alertFailureCount == 2);
     printf("All is well (maybe!)\n");
     return 0;
 }
